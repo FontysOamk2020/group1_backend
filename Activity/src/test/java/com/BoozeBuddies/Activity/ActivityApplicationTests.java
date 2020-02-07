f1849e1e9f04da0686b8a23a1e74494318d1328f
wiebj3 <wiebj3@gmail.com>
2020-02-07 10:46:20 +0100

    Added all services

---
+++ Activity/src/test/java/com/BoozeBuddies/Activity/ActivityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.BoozeBuddies.Activity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ActivityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
