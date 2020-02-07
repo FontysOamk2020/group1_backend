c9cfc3f0d47a8182ccf041ab0f5196cff0640757
MarioHendriks <43466121+MarioHendriks@users.noreply.github.com>
2020-02-07 10:54:36 +0200

    started with endpoints

---
+++ User/src/main/java/com/BoozeBuddies/User/Resources/UserResource.java
@@ -0,0 +1,34 @@
+package com.BoozeBuddies.User.Resources;
+
+import com.BoozeBuddies.User.Models.User;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping(value = "api/public/user")
+public class UserResource {
+
+    @GetMapping(value = "/getUserByUserId/{id}")
+    User GetUserByUserId(@PathVariable("id")int id){
+        return null;
+    }
+
+    @GetMapping(value = "/getUserByEmail/{email}")
+    User GetUserByUserEmail(@PathVariable("email")String email){
+        return null;
+    }
+
+    @PutMapping(value = "/UpdateUsername")
+    User UpdateUsername(){
+        return null;
+    }
+
+    @PostMapping(value = "/addUser")
+    User AddUser(){
+        return null;
+    }
+
+    @DeleteMapping(value = "/deleteUser")
+    User DeleteUser(){
+        return null;
+    }
+}
