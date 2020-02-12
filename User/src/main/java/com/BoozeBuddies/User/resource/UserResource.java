91fb7a4b383cf1dcee3657868e63f98be1e8bd59
WiebevanHerwijnen <32171131+WiebevanHerwijnen@users.noreply.github.com>
2020-02-12 10:51:10 +0100

    Update UserResource.java

--- User/src/main/java/com/BoozeBuddies/User/resource/UserResource.java
+++ User/src/main/java/com/BoozeBuddies/User/resource/UserResource.java
@@ -1,6 +1,7 @@
 package com.BoozeBuddies.User.resource;
 
 import com.BoozeBuddies.User.dal.repository.UserRepo;
+import com.BoozeBuddies.User.logic.UserLogic;
 import com.BoozeBuddies.User.model.User;
 import org.springframework.web.bind.annotation.*;
 
@@ -13,30 +14,30 @@ public class UserResource {
     @GetMapping(value = "/getUserByUserId/{id}")
     User GetUserByUserId(@PathVariable("id")int id)
     {
-        return UserRepo.GetUserById(id);
+        return UserLogic.GetUserById(id);
     }
 
     @GetMapping(value = "/getUserByEmail/{email}")
     User GetUserByEmail(@PathVariable("email")String email)
     {
-        return UserRepo.GetUserByEmail(email);
+        return UserLogic.GetUserByEmail(email);
     }
 
     @PutMapping(value = "/UpdateUsername")
     User UpdateUsername(User user)
     {
-        return UserRepo.UpdateUsername(user);
+        return UserLogic.UpdateUsername(user);
     }
 
     @PostMapping(value = "/addUser")
     User AddUser(User user)
     {
-        return UserRepo.AddUser(user);
+        return UserLogic.AddUser(user);
     }
 
     @DeleteMapping(value = "/deleteUser")
     User DeleteUser(User user)
     {
-        return UserRepo.DeleteUser(user);
+        return UserLogic.DeleteUser(user);
     }
 }
