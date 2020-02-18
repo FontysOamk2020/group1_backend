b07aef51269c464390ab42382d4d0ad5f2be55ce
MarioHendriks <43466121+MarioHendriks@users.noreply.github.com>
2020-02-18 17:42:13 +0100

    Update UserResource.java

--- User/src/main/java/com/BoozeBuddies/User/resource/UserResource.java
+++ User/src/main/java/com/BoozeBuddies/User/resource/UserResource.java
@@ -17,43 +17,34 @@ public class UserResource {
 
     UserCollectionLogic userCollectionLogic = new UserCollectionLogic(new UserRepo(new UserContextHibernate()));
     UserLogic userLogic = new UserLogic(new UserRepo(new UserContextHibernate()));
-
-    @CrossOrigin(origins = {"*"})
+    
     @GetMapping(value = "/getUserByUserId/{id}")
     User GetUserById(@PathVariable("id")int id)
     {
         return userCollectionLogic.getUserById(id);
     }
 
-    @CrossOrigin(origins = {"*"})
     @GetMapping(value = "/getUserByEmail/{email}")
     User GetUserByEmail(@PathVariable("email")String email)
     {
         return userCollectionLogic.getUserByEmail(email);
     }
 
-    @CrossOrigin(origins = {"*"})
     @PutMapping(value = "/UpdateUsername")
     User UpdateUsername(@RequestBody UpdateUser updateUser)
     {
         return userLogic.updateUsername(updateUser);
     }
 
-    @CrossOrigin(origins = {"*"})
     @PostMapping(value = "/addUser")
     User AddUser(@RequestBody User user)
     {
         return userCollectionLogic.addUser(user);
     }
 
-    @CrossOrigin(origins = {"*"})
     @DeleteMapping(value = "/deleteUser")
-    boolean DeleteUser(@RequestBody User user)
-    {
-         return userLogic.deleteUser(user);
-    }
+    boolean DeleteUser(@RequestBody User user) { return userLogic.deleteUser(user); }
 
-    @CrossOrigin(origins = {"*"})
     @GetMapping(value = "/getAllUsers", produces = MediaType.APPLICATION_JSON_VALUE)
     List<User> getAllUsers()
     {
