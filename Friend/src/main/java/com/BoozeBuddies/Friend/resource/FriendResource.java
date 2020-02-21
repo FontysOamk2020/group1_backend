03d3a298924716349ba12da6d291d9a277e3871c
MarioHendriks <43466121+MarioHendriks@users.noreply.github.com>
2020-02-21 14:47:15 +0100

    Update FriendResource.java

--- Friend/src/main/java/com/BoozeBuddies/Friend/resource/FriendResource.java
+++ Friend/src/main/java/com/BoozeBuddies/Friend/resource/FriendResource.java
@@ -3,7 +3,9 @@ package com.BoozeBuddies.Friend.resource;
 import com.BoozeBuddies.Friend.Dal.Context.FriendCollectionHibernateContext;
 import com.BoozeBuddies.Friend.Logic.FriendCollectionLogic;
 import com.BoozeBuddies.Friend.Logic.FriendLogic;
+import com.BoozeBuddies.Friend.Models.Friend;
 import com.BoozeBuddies.Friend.Models.RelationshipCollection;
+import com.BoozeBuddies.Friend.Models.RequestRelationship;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -13,9 +15,24 @@ public class FriendResource {
     FriendCollectionLogic friendCollectionLogic = new FriendCollectionLogic(new FriendCollectionHibernateContext());
     FriendLogic friendLogic = new FriendLogic(new FriendCollectionHibernateContext());
 
-    @RequestMapping(path = "/getFriendsByUserId/{id}")
+    @GetMapping(path = "/getFriendsByUserId/{id}")
     RelationshipCollection GetAllFriendsById(@PathVariable("id") int id){
         return friendCollectionLogic.getFriendsById(id);
+    }
+
+    @PutMapping(path = "/updateRelationshipStatus")
+    Boolean UpdateRelationshipStatus(@RequestBody Friend updateRelationshipStatus){
+        return friendLogic.changeRelationStatus(updateRelationshipStatus);
+    }
+
+    @PostMapping(path = "/addRelationship")
+    Boolean AddRelationship(@RequestBody RequestRelationship requestRelationship){
+        return friendCollectionLogic.AddRelationship(requestRelationship);
+    }
+
+    @DeleteMapping(path = "/deleteRelationship")
+    Boolean DeleteRelationship(@RequestBody Friend updateRelationshipStatus){
+        return friendCollectionLogic.DeleteRelationship(updateRelationshipStatus);
     }
 
 }
