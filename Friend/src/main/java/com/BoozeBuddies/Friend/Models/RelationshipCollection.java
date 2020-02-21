efb98033d1d3874cf6062bb6ce7201c544d47df2
MarioHendriks <43466121+MarioHendriks@users.noreply.github.com>
2020-02-21 14:48:26 +0100

    Update RelationshipCollection.java

--- Friend/src/main/java/com/BoozeBuddies/Friend/Models/RelationshipCollection.java
+++ Friend/src/main/java/com/BoozeBuddies/Friend/Models/RelationshipCollection.java
@@ -1,29 +1,51 @@
 package com.BoozeBuddies.Friend.Models;
 
-import java.util.ArrayList;
-import java.util.List;
+import com.mysql.cj.x.protobuf.MysqlxDatatypes;
+
+import java.util.*;
 
 public class RelationshipCollection
 {
-    private List<Relationship> relationships;
+    private List<Friend> relationships;
+    private List<Friend> acceptedFriends;
+    private List<Friend> pendingFriends;
+    private List<Friend> blockedFriends;
 
-    public RelationshipCollection()
-    {
-        relationships = new ArrayList<>();
-    }
 
-    public RelationshipCollection(List<Relationship> relationships)
-    {
-        this.relationships = relationships;
-    }
+    public List<Friend> getAcceptedFriends() { return acceptedFriends; }
 
-    public List<Relationship> getRelationships()
+    public void setAcceptedFriends(List<Friend> acceptedFriends) { this.acceptedFriends = acceptedFriends; }
+
+    public List<Friend> getPendingFriends() { return pendingFriends; }
+
+    public void setPendingFriends(List<Friend> pendingFriends) { this.pendingFriends = pendingFriends; }
+
+    public List<Friend> getBlockedFriends() { return blockedFriends; }
+
+    public void setBlockedFriends(List<Friend> blockedFriends) { this.blockedFriends = blockedFriends; }
+
+    public List<Friend> getRelationships()
     {
         return relationships;
     }
 
-    public void setRelationships(List<Relationship> relationships)
+    public void setRelationships(List<Friend> relationships)
     {
         this.relationships = relationships;
     }
+
+    public RelationshipCollection()
+    {
+        relationships = new ArrayList<>();
+        acceptedFriends = new ArrayList<>();
+        pendingFriends = new ArrayList<>();
+        blockedFriends = new ArrayList<>();
+    }
+
+    public RelationshipCollection(List<Friend> relationships)
+    {
+        this.relationships = relationships;
+    }
+
+
 }
