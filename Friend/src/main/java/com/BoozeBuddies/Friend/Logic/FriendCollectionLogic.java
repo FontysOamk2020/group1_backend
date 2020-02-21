08ac2b0977ad1ca522454be6ac65d68579047d77
MarioHendriks <43466121+MarioHendriks@users.noreply.github.com>
2020-02-21 14:47:55 +0100

    logic

--- Friend/src/main/java/com/BoozeBuddies/Friend/Logic/FriendCollectionLogic.java
+++ Friend/src/main/java/com/BoozeBuddies/Friend/Logic/FriendCollectionLogic.java
@@ -1,21 +1,48 @@
 package com.BoozeBuddies.Friend.Logic;
 import com.BoozeBuddies.Friend.Dal.Interfaces.IFriendCollectionRepo;
 import com.BoozeBuddies.Friend.Dal.Interfaces.IFriendRepo;
+import com.BoozeBuddies.Friend.Models.Enums.FriendStatus;
+import com.BoozeBuddies.Friend.Models.Friend;
 import com.BoozeBuddies.Friend.Models.RelationshipCollection;
+import com.BoozeBuddies.Friend.Models.RequestRelationship;
+import com.BoozeBuddies.Friend.Models.User;
+import com.BoozeBuddies.Friend.Utils.ApiRequest;
 import com.BoozeBuddies.Friend.factory.FriendFactory;
 import org.springframework.lang.Nullable;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class FriendCollectionLogic {
     private FriendFactory friendFactory = new FriendFactory();
-    private IFriendCollectionRepo friendCollectionRepo = friendFactory.createUserCollectionRepo();
+    private IFriendCollectionRepo friendCollectionRepo;
 
-    public FriendCollectionLogic(@Nullable IFriendCollectionRepo repo)
-    {
+    public FriendCollectionLogic(@Nullable IFriendCollectionRepo repo) {
         friendCollectionRepo = ((repo == null) ? friendFactory.createUserCollectionRepo() : repo);
     }
 
     public RelationshipCollection getFriendsById(int id) {
+        //TODO check for unique relations
         return friendCollectionRepo.getFriendsById(id);
     }
+
+    public boolean AddRelationship(RequestRelationship requestRelationship) {
+        //TODO add check if user is already a friend
+        Friend friend = new Friend();
+        friend.setUserOneId(requestRelationship.getYou());
+
+        String url = "http://217.101.44.31:8081/api/public/user/getUserByUserId/"+requestRelationship.getFriendID();
+        User friendUser = (User)ApiRequest.CreateGetRequest(url, new User());
+
+        friend.setStatus(FriendStatus.pending);
+        friend.setUserTwoId(friendUser);
+
+        return friendCollectionRepo.AddRelationship(friend);
+    }
+
+    public boolean DeleteRelationship(Friend relationship) {
+        return friendCollectionRepo.DeleteRelationship(relationship);
+    }
 }
+
--- Friend/src/main/java/com/BoozeBuddies/Friend/Logic/FriendLogic.java
+++ Friend/src/main/java/com/BoozeBuddies/Friend/Logic/FriendLogic.java
@@ -1,17 +1,22 @@
 package com.BoozeBuddies.Friend.Logic;
 
-import com.BoozeBuddies.Friend.Dal.Interfaces.IFriendCollectionRepo;
+import com.BoozeBuddies.Friend.Dal.Interfaces.IFriendRepo;
+import com.BoozeBuddies.Friend.Models.Friend;
 import com.BoozeBuddies.Friend.factory.FriendFactory;
 import org.springframework.lang.Nullable;
 
 
 public class FriendLogic {
     private FriendFactory friendFactory = new FriendFactory();
-    private IFriendCollectionRepo friendCollectionRepo = friendFactory.createUserCollectionRepo();
+    private IFriendRepo friendRepo;
 
-    public FriendLogic(@Nullable IFriendCollectionRepo repo)
+    public FriendLogic(@Nullable IFriendRepo repo)
     {
-        friendCollectionRepo = ((repo == null) ? friendFactory.createUserCollectionRepo() : repo);
+        friendRepo = ((repo == null) ? friendFactory.createUserRepo() : repo);
+    }
+
+    public boolean changeRelationStatus(Friend updateRelationship){
+        return friendRepo.UpdateRelationship(updateRelationship);
     }
 
 }
