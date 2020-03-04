9ab5187353ff1047c2ae34f65b6cd37a558059b2
coecomber <joostvherwaarden@hotmail.com>
2020-03-04 10:31:52 +0100

    Development host ready

--- Friend/src/main/java/com/BoozeBuddies/Friend/resource/FriendResource.java
+++ Friend/src/main/java/com/BoozeBuddies/Friend/resource/FriendResource.java
@@ -15,21 +15,25 @@ public class FriendResource {
     FriendCollectionLogic friendCollectionLogic = new FriendCollectionLogic(new FriendCollectionHibernateContext());
     FriendLogic friendLogic = new FriendLogic(new FriendCollectionHibernateContext());
 
+    @CrossOrigin(origins = {"*"})
     @GetMapping(path = "/getFriendsByUserId/{id}")
     RelationshipCollection GetAllFriendsById(@PathVariable("id") int id){
         return friendCollectionLogic.getFriendsById(id);
     }
 
+    @CrossOrigin(origins = {"*"})
     @PutMapping(path = "/updateRelationshipStatus")
     Boolean UpdateRelationshipStatus(@RequestBody Friend updateRelationshipStatus){
         return friendLogic.changeRelationStatus(updateRelationshipStatus);
     }
 
+    @CrossOrigin(origins = {"*"})
     @PostMapping(path = "/addRelationship")
     Boolean AddRelationship(@RequestBody RequestRelationship requestRelationship){
         return friendCollectionLogic.AddRelationship(requestRelationship);
     }
 
+    @CrossOrigin(origins = {"*"})
     @DeleteMapping(path = "/deleteRelationship")
     Boolean DeleteRelationship(@RequestBody Friend updateRelationshipStatus){
         return friendCollectionLogic.DeleteRelationship(updateRelationshipStatus);
